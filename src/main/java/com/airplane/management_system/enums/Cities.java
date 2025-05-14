37976df171dda9893d3b9503607b41926b95a1c0
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-05-14 22:48:02 +0400

    Available cities Added!

---
+++ src/main/java/com/airplane/management_system/enums/Cities.java
@@ -0,0 +1,24 @@
+package com.airplane.management_system.enums;
+
+public enum Cities {
+
+    BAKU,
+    MOSCOW,
+    BERLIN,
+    LONDON,
+    ZURICH,
+    PARIS,
+    SYDNEY,
+    TOKIO,
+    ROME,
+    CALIFORNIA,
+    LOS_ANGELES,
+    MADRID,
+    LISBON,
+    DUBLIN,
+    KIEV,
+    TBILISI,
+    ISTANBUL,
+    ANKARA
+
+}
