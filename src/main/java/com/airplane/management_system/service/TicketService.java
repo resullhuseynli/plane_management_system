4790563cfda32419a83b0fa0036f14af956ba443
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-05-14 23:16:43 +0400

    TicketService added!

---
+++ src/main/java/com/airplane/management_system/service/TicketService.java
@@ -0,0 +1,10 @@
+package com.airplane.management_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TicketService {
+
+
+
+}
---
+++ src/main/java/com/airplane/management_system/service/serviceImpl/TicketServiceImpl.java
@@ -0,0 +1,6 @@
+package com.airplane.management_system.service.serviceImpl;
+
+import com.airplane.management_system.service.TicketService;
+
+public class TicketServiceImpl implements TicketService {
+}
