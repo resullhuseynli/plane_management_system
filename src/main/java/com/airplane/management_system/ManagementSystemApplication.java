fe85591ae890daf678067eb4d14c364f63a5dc73
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-05-14 22:48:17 +0400

    @EntityScan annotation added

--- src/main/java/com/airplane/management_system/ManagementSystemApplication.java
+++ src/main/java/com/airplane/management_system/ManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.airplane.management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan
 public class ManagementSystemApplication {
 
 	public static void main(String[] args) {
